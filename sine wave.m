e520b35a475bff98163a76b8c1bbe0a91a4ce0a2
Shreyas1308 <56815931+Shreyas1308@users.noreply.github.com>
2021-07-24 18:07:14 +0530

    Delete sine wave.m

--- sine wave.m
+++
@@ -1,6 +0,0 @@
-fs = 512;                    % Sampling frequency (samples per second)
-dt = 1/fs;                   % seconds per sample
-StopTime = 0.25;             % seconds
-t = (0:dt:StopTime-dt)';     % seconds
-F = 60;                      % Sine wave frequency (hertz)
-data = sin(2*pi*F*t);
